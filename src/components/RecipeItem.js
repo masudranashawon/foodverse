9509e24e381b31157d95ec7361aeacc84baa719a
masudranashawon <mohammadmasudrana3396@gmail.com>
2022-12-23 18:17:51 +0600

    recipe item fetch & some markup are ready

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -5,8 +5,51 @@ const RecipeItem = () => {
   const { id } = useParams();
 
   const { data: recipe, loading, error } = useFetch(id);
+
+  const durationCalc = (duration) => {
+    if (!duration) return;
+
+    if (!String(duration).includes(".")) return duration + "h";
+
+    if (String(duration).includes("."))
+      return String(duration).replace(".", "h") + "min";
+  };
+
   console.log(recipe);
-  return <div>RecipeItem</div>;
+
+  return (
+    <div className='recipe-item-section container mx-auto py-20 grid grid-cols-1 lg:grid-cols-2 gap-10'>
+      <div className='left-sec'>
+        <div className='img'>
+          <img src={recipe?.image_url} alt={recipe?.title} />
+        </div>
+        <div className='ingredients'>
+          <span className='ing-title'>Ingredients</span>
+          <ul>
+            {recipe?.ingredients?.map((ing, i) => (
+              <li key={i}>
+                ✔ {ing.quantity}
+                {ing.unit} {ing.description}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className='right-sec'>
+        <span className='publisher'>{recipe?.publisher}</span>
+        <h2 className='title'>{recipe?.title}</h2>
+        <div className='servings-cooking-time'>
+          <div className='servings'>Servings (People): {recipe?.servings}</div>
+          <div className='cooking-time'>
+            Cooking time:{" "}
+            {recipe?.cooking_time < 60
+              ? String(recipe?.cooking_time) + "min"
+              : durationCalc(recipe?.cooking_time / 60)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RecipeItem;
